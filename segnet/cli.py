495121e626e85bf568afbd5aea31156130820dc2
Maggie Cai <peiwenc@live.unc.edu>
2018-03-30 11:36:50 -0400

    Added plot_histogram_fdist.

--- segnet/cli.py
+++ segnet/cli.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
@@ -5,7 +6,9 @@ import click
 
 from . import segnet
 from . import utils
+from collections import defaultdict, OrderedDict
 from . import __logo_text__, __version__
+
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -51,25 +54,35 @@ def diffuse(netfile, seedfile, multiseeds, gidfile, outdir, verbose):
     else:
         idmap = None
     pos_seeds, neg_seeds = segnet.prepare_seeds(seedfile)
-    network = segnet.readin_network(netfile, idmap)
+    network=segnet.refine_network(netfile,idmap)
     if multiseeds:
         output = segnet.diffuse_multi_seeds(network, pos_seeds, neg_seeds, outdir)
+        #segnet.draw_modules(network, network.nodes(), output)
+        #print("after diffuse_multi_seeds, printing output")
+        #print(output)
+        #segnet.plot_histogram(output, outfile="test_histo.png")
+        #print("after plot_histogram, now drawing network")
+        segnet.draw_modules(network, network.nodes(), network.edges(),outdir)
+        print("after draw_modules")
+        segnet.plot_histogram_fdist(output, outfile="test_histo.png")
+        segnet.plot_histogram_per_seed(output, outfile="test_histo_perseed.png")
     else:
         print("before segnet.diffuse_single_seed in cli.py")
-        #output = segnet.diffuse_single_seed(network, pos_seeds, neg_seeds, outdir)
-        output = segnet.diffuse_single_seed(network, pos_seeds, neg_seeds, outdir=None)
+
+        
         print("after diffuse_single_seed, printing output")
-        #print(output)
-        segnet.plot_histogram(output, outfile="test2.png")
+        #print(output)     
+        segnet.plot_histogram_fdist(output, outfile="test_histo.png")
+        #segnet.plot_histogram_per_seed(output, outfile="test_histo_perseed.png")
         print("after plot_histogram, now drawing network")
-        segnet.draw_modules(network, network.nodes())
+        segnet.draw_modules(network, network.nodes(), network.edges(), outdir)
         print("after draw_modules")
 
         #f = open(os.path.join(outdir, 'results.pickled', 'wb')
         #cPickle.dump(all_results, f)
         #f.close()
         #segnet.plot_histogram(output)
-
+    
 
 
 if __name__ == "__main__":
